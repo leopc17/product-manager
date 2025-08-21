5ae0513838892c7c81d76908c074b0604a1ccbbb
leopc17 <LeonardoEAF45@gmail.com>
2025-08-21 14:10:29 -0300

    feat(product): adiciona anotações de validação dos dados no DTO de requisição

--- src/main/java/com/br/productmanager/dto/ProductRequestDto.java
+++ src/main/java/com/br/productmanager/dto/ProductRequestDto.java
@@ -1,12 +1,22 @@
 package com.br.productmanager.dto;
 
 import com.br.productmanager.enums.ProductCategory;
+import jakarta.validation.constraints.*;
 
 import java.math.BigDecimal;
 
 public record ProductRequestDto (
+    @NotBlank(message = "name can't be empty")
+    @Size(max = 150, message = "name size must be less than 150")
     String name,
+
+    @PositiveOrZero(message = "price must be greater than zero")
+    @Digits(integer = 10, fraction = 2, message = "price must have at most 10 integer digits and 2 decimal places.")
     BigDecimal price,
+
     String description,
+
+    @NotNull(message = "product category is required")
     ProductCategory category
-    ) { }
+
+) {}
