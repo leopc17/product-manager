9536124f98c94c763a65e8991fd1bbf6a9799d61
leopc17 <LeonardoEAF45@gmail.com>
2025-08-29 14:55:05 -0300

    feat: cria anotação de validação personalizada para enums

---
+++ src/main/java/com/br/productmanager/validation/validators/EnumValueValidator.java
@@ -0,0 +1,35 @@
+package com.br.productmanager.validation.validators;
+
+import com.br.productmanager.validation.annotation.EnumValue;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class EnumValueValidator implements ConstraintValidator<EnumValue, String> {
+
+    private Enum<?>[] enumValues;
+
+    @Override
+    public void initialize(EnumValue constraintAnnotation) {
+        Class<? extends Enum<?>> enumClass = constraintAnnotation.enumClass();
+        enumValues = enumClass.getEnumConstants();
+    }
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext constraintValidatorContext) {
+        if (value == null) {
+            return false;
+        }
+
+        if (value.isBlank()) {
+            return false;
+        }
+
+        for (Enum<?> enumValue : enumValues) {
+            if (enumValue.name().equalsIgnoreCase(value)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
